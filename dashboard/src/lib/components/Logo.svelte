70725be816410a24a3114407790b5a2ee0d72b5c
Malted <malted@malted.dev>
2023-12-10 18:42:44 +0000

    🐛 Base path bugs

--- dashboard/src/lib/components/Logo.svelte
+++ dashboard/src/lib/components/Logo.svelte
@@ -1,4 +1,8 @@
-<a href="/" class="flex items-center text-2xl">
+<script lang="ts">
+	import { base } from "$lib/utils";
+</script>
+
+<a href="{base}/" class="flex items-center text-2xl">
 	PM
 	<span class="font-bold">RS</span>
 </a>
--- dashboard/src/lib/components/Navbar.svelte
+++ dashboard/src/lib/components/Navbar.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import Logo from "./Logo.svelte";
+	import { base } from "$lib/utils";
 </script>
 
 <nav class="flex justify-between p-3 bg-zinc-100 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100">
 	<div class="flex items-center gap-4">
 		<Logo />
-		<a href="/" class="ml-4">Services</a>
-		<a href="/system">System</a>
+		<a href="{base}/" class="ml-4">Services</a>
+		<a href="{base}/system">System</a>
 	</div>
 
 	<ul>
@@ -19,7 +20,7 @@
 			<div
 				class="absolute w-5 rounded-full bg-zinc-100 dark:bg-zinc-800 bottom-[-10%] left-[-10%]"
 			>
-				<img src="/icons/github.svg" alt="github icon" class="dark:invert" />
+				<img src="{base}/icons/github.svg" alt="github icon" class="dark:invert" />
 			</div>
 		</div>
 	</ul>
